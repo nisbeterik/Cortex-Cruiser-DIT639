f840df10a991fe0de2909ace19211e0b505efcf9
Erik Nisbet <eriknisbet8@gmail.com>
2025-04-23 14:37:38 +0200

    #12 Go back to alpine

--- cpp-opencv/src/template-opencv.cpp
+++ cpp-opencv/src/template-opencv.cpp
@@ -19,7 +19,6 @@
 #include "cluon-complete.hpp"
 // Include the OpenDLV Standard Message Set that contains messages that are usually exchanged for automotive or robotic applications 
 #include "opendlv-standard-message-set.hpp"
-#include "template-opencv.hpp"
 // Include the GUI and image processing header files from OpenCV
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
