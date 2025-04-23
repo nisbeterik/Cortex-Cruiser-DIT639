71f92680cf4b565ed084c7451e82db7fb1f7d456
Erik Nisbet <eriknisbet8@gmail.com>
2025-04-23 12:58:55 +0200

    #12 Add more settings in CMakeLists.txt

--- cpp-opencv/src/test-template.cpp
+++ cpp-opencv/src/test-template.cpp
@@ -1,7 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "template-opencv.hpp"
-#include "cluon-complete.hpp"
 
 TEST_CASE("isTrue returns true", "[template-opencv]") {
     REQUIRE(isTrue() == true);
