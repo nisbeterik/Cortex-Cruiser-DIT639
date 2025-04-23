33d9fcc267f326baa79d106d8471a3bb2897bade
Erik Nisbet <eriknisbet8@gmail.com>
2025-04-23 12:45:35 +0200

    #12 Add dependency in test-template.cpp

--- cpp-opencv/src/test-template.cpp
+++ cpp-opencv/src/test-template.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "template-opencv.hpp"
+#include "cluon-complete.hpp"
 
 TEST_CASE("isTrue returns true", "[template-opencv]") {
     REQUIRE(isTrue() == true);
