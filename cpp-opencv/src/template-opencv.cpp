8340a21910b9a874b3fea785c7e880be4b5cc8b1
Ubuntu <eriknis@student.chalmers.se>
2025-05-06 14:19:42 +0200

    #14 Comment out unwanted prints

--- cpp-opencv/src/template-opencv.cpp
+++ cpp-opencv/src/template-opencv.cpp
@@ -94,7 +94,7 @@ int32_t main(int32_t argc, char **argv)
                 // https://github.com/chrberger/libcluon/blob/master/libcluon/testsuites/TestEnvelopeConverter.cpp#L31-L40
                 std::lock_guard<std::mutex> lck(gsrMutex);
                 gsr = cluon::extractMessage<opendlv::proxy::GroundSteeringRequest>(std::move(env));
-                std::cout << "lambda: groundSteering = " << gsr.groundSteering() << std::endl;
+                //std::cout << "lambda: groundSteering = " << gsr.groundSteering() << std::endl;
             };
 
             od4.dataTrigger(opendlv::proxy::GroundSteeringRequest::ID(), onGroundSteeringRequest);
@@ -159,10 +159,10 @@ int32_t main(int32_t argc, char **argv)
                 std::cout << "group_06; " << ts_ms << "; " << steeringAngle << direction << std::endl;
 
                 // If you want to access the latest received ground steering, don't forget to lock the mutex:
-                {
-                    std::lock_guard<std::mutex> lck(gsrMutex);
-                    std::cout << "main: groundSteering = " << gsr.groundSteering() << "ts: " << ts_ms << std::endl;
-                }
+//                {
+    //                std::lock_guard<std::mutex> lck(gsrMutex);
+  //                  std::cout << "main: groundSteering = " << gsr.groundSteering() << "ts: " << ts_ms << std::endl;
+ //               }
 
                 // Display image on your screen.
                 if (VERBOSE)
