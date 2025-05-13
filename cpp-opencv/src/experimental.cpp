041977429daaf63d16e7c255a8578257b0261f87
Erik Nisbet <eriknisbet8@gmail.com>
2025-05-13 17:23:36 +0200

    #18 Add new experimental cpp file

---
+++ cpp-opencv/src/experimental.cpp
@@ -0,0 +1,19 @@
+#include "cluon-complete.hpp"
+// Include the OpenDLV Standard Message Set that contains messages that are usually exchanged for automotive or robotic applications
+#include "opendlv-standard-message-set.hpp"
+// Include the GUI and image processing header files from OpenCV
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <fstream>
+
+// Include more libraries
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+int32_t main(int32_t argc, char **argv) {
+    std::cout << "test";
+}
