3470e1a171860a92fe683287c7e17bf53c85e74e
Ubuntu <eriknis@student.chalmers.se>
2025-05-13 18:59:37 +0200

    #18 Add comments for explanation

--- cpp-opencv/src/experimental.cpp
+++ cpp-opencv/src/experimental.cpp
@@ -10,12 +10,15 @@
 #include <string>
 #include <iomanip>
 
+
+// booleans for Player parameters
 constexpr const bool AUTOREWIND{false};
 constexpr const bool THREADING{false};
 
 int32_t main(int32_t argc, char **argv) {
     auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
 
+    // parse recording file path in arguments and --verbose flag
     if (commandlineArguments.count("rec") == 0) {
         std::cerr << argv[0] << " requires a recording file to process." << std::endl;
         std::cerr << "Usage:   " << argv[0] << " --rec=<Recording.rec> [--verbose]" << std::endl;
@@ -23,43 +26,45 @@ int32_t main(int32_t argc, char **argv) {
         return 1;
     }
 
-    const std::string recFile = commandlineArguments["rec"];
+    const std::string recFile = commandlineArguments["rec"]; 
     bool verbose = (commandlineArguments.count("verbose") != 0);
 
     if (verbose) {
         std::cout << "Processing recording file: " << recFile << std::endl;
     }
 
-    cluon::Player player(recFile, AUTOREWIND, THREADING);
+    cluon::Player player(recFile, AUTOREWIND, THREADING); // pass recording file and other parameters to Player object
 
 
-    std::cout << "Recording contains " << player.totalNumberOfEnvelopesInRecFile() << " messages" << std::endl;
-    opendlv::proxy::GroundSteeringRequest gsr;
-    opendlv::proxy::ImageReading ir;
-    cluon::data::TimeStamp ts;
-    int64_t ts_ms;
-    int32_t lineCount = 0;
-    bool hasImage = false;
+    opendlv::proxy::GroundSteeringRequest gsr; // variable to store gsr message
+    opendlv::proxy::ImageReading ir; // variable to store imagereading message
+    cluon::data::TimeStamp ts; // TimeStamp object to store timestamp
+    int64_t ts_ms; // variable to store timestamp in millieseconds
+    int32_t lineCount = 0; // line count to make the number of prints matches number of groundsteering messages
+    bool hasImage = false; // variable to keep track if image frame has equivalent gsr data
+    
+
+    // loop that ends when .rec file has no more data
+
     while (player.hasMoreData()) {
-        auto next = player.getNextEnvelopeToBeReplayed();
+        auto next = player.getNextEnvelopeToBeReplayed(); // get next envelope of .rec file
         if (next.first) {
-            cluon::data::Envelope envelope = next.second;
+            cluon::data::Envelope envelope = next.second; // store current envelope
             
             if (verbose) {
-               
 
-                // std::cout << "Received envelope with ID: " << envelope.dataType() << " at " << ts_ms << std::endl;
+                // if datatype is ImageReading (see opendlv-standard-message-set)
                 if(envelope.dataType() == 1055) {
                     ts = envelope.sampleTimeStamp();
-                    ts_ms = cluon::time::toMicroseconds(ts);
-                    ir = cluon::extractMessage<opendlv::proxy::ImageReading>(std::move(envelope)); 
+                    ts_ms = cluon::time::toMicroseconds(ts); // take timestamp
+                    ir = cluon::extractMessage<opendlv::proxy::ImageReading>(std::move(envelope)); // extract message into ir var
                     hasImage = true;
-                    // std::cout << "ir width " << ir.width() << std::endl; // should be ir.data()
-                } else if (envelope.dataType() == 1090) {
+                } else if (envelope.dataType() == 1090) { // if datatype is GroundSteeringRequest (see: opendlv-standard-message-set)
+                    // if corresponding image exists with timestamp
                     if(hasImage) {
                         gsr = cluon::extractMessage<opendlv::proxy::GroundSteeringRequest>(std::move(envelope));
                         std::cout << "group_06;" << ts_ms << ";" << gsr.groundSteering() << ";" << ir.width() << std::endl;
-                        lineCount++;
+                        // lineCount++;
                         hasImage = false;
                     }
                 }
